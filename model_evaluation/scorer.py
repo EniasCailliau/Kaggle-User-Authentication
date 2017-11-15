ad061d6900ab19078ffc8b9da11bd52f8c0f1343
Jeroen Van Hautte <Jeroen@solidsystems.local>
2017-11-15 11:52:51 +0100

    gitignore edit, ran test for RF model

--- model_evaluation/scorer.py
+++ model_evaluation/scorer.py
@@ -3,7 +3,6 @@ from sklearn.metrics import roc_auc_score
 
 from utils import handyman
 
-
 def auc_evaluator(estimator, X, y):
     y_scores = estimator.predict_proba(X)
     y_one_hot = handyman.to_one_hot([int(x) for x in y], min_int=1, max_int=8)
