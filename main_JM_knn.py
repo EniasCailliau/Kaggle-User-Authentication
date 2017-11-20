b4990737b4d17ae35fc4ac0c69d14d7dd0cd5ca4
Joppe <joppem13@hotmail.com>
2017-11-20 13:30:51 +0100

    KNN and Decision tree models

---
+++ main_JM_knn.py
@@ -0,0 +1,28 @@
+import trainer
+import os
+from models import nearest_neighbours
+from model_evaluation import scorer, visualiser
+from feature_reduction import feature_reducer
+from utils import pandaman
+
+options = ["nearest_neighbours", "test"]
+results_location = os.path.join("Results", '/'.join(options) + "/")
+
+trainer = trainer.Trainer()
+
+estimator = nearest_neighbours.KNN(n_neighbors=5)
+
+train_features, train_activity_labels, train_subject_labels, test_features = trainer.load_data(
+    os.path.join("feature_extraction", '_data_sets/unreduced.pkl'), final=False)
+
+pandaman.print_stats(train_features=train_features, train_activity_labels=train_activity_labels,
+                     train_subject_labels=train_subject_labels, test_features=test_features)
+
+tuned_parameters = [
+    {'n_neighbors': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+]
+
+trainer.find_optimized_model(estimator, train_features, train_subject_labels, tuned_parameters, scorer.auc_evaluator)
+
+trainer.evaluate(estimator, train_features, train_subject_labels, scorer.auc_evaluator, results_location)
+visualiser.plot_learning_curves(estimator, train_features, train_subject_labels, results_location)
