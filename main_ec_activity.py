8d38228fe65ddaf623d58733d39fef5e032f7a49
Enias Cailliau <enias.cailliau@outlook.com>
2017-11-21 11:45:51 +0100

    Fixed class imbalance during plotting learning curve

--- main_ec_activity.py
+++ main_ec_activity.py
@@ -5,11 +5,9 @@ from feature_reduction import feature_reducer
 from model_evaluation import scorer
 from model_evaluation import visualiser
 from models.activity_prediction import gaussianNB
-from utils import pandaman
+from utils import pandaman, handyman
 
 options = ["ec", "activity", "gaussian", "reduction_LDA"]
-
-results_location = os.path.join("Results", '/'.join(options) + "/")
 
 trainer = trainer.Trainer()
 
@@ -25,17 +23,21 @@ print("Reducing using variance")
 train_features_reduced = feature_reducer.reduce_variance(train_features, p=0.95)
 pandaman.print_stats(train_features_reduced=train_features_reduced)
 
-for i in range(1, 40, 2):
+for i in range(5, 40, 2):
     print("Starting step: {}".format(i))
     print("Reducing using LDA")
     train_features_reduced = feature_reducer.reduce_LDA(train_features, train_activity_labels, n_components=i)
     pandaman.print_stats(train_features_reduced=train_features_reduced)
 
     print("------------------------------------------------------------------------")
+    options.append(str(i))
+    results_location = handyman.calculate_path_from_options("Results", options)
+    options.pop()
+
+    handyman.calculate_path_from_options("Results", options)
+
     trainer.evaluate(estimator, train_features_reduced, train_activity_labels, scorer.accuracy_evaluator,
                      results_location)
-    options.append(str(i))
     visualiser.plot_learning_curves(estimator, train_features, train_activity_labels, results_location)
     trainer.save_estimator(estimator, results_location)
     estimator = trainer.load_estimator(results_location)
-    options.pop()
