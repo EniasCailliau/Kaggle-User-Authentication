df0e4947ee71a2dd9cb90959f311a3b5b4cef08f
Joppe <joppem13@hotmail.com>
2017-11-21 20:54:30 +0100

    fixed spectral centroid, DT visualisation

--- main_JM_knn.py
+++ main_JM_knn.py
@@ -5,24 +5,27 @@ from model_evaluation import scorer, visualiser
 from feature_reduction import feature_reducer
 from utils import pandaman
 
-options = ["nearest_neighbours", "test"]
-results_location = os.path.join("Results", '/'.join(options) + "/")
 
-trainer = trainer.Trainer()
+if __name__ == "__main__":
+    options = ["nearest_neighbours", "test"]
+    results_location = os.path.join("Results", '/'.join(options) + "/")
 
-estimator = nearest_neighbours.KNN(n_neighbors=5)
+    trainer = trainer.Trainer()
 
-train_features, train_activity_labels, train_subject_labels, test_features = trainer.load_data(
-    os.path.join("feature_extraction", '_data_sets/unreduced.pkl'), final=False)
+    for k in (5,6,7,8,9,10,11,12,13,14,15):
+        string = "Results for value k = "+str(k)
+        print(string)
 
-pandaman.print_stats(train_features=train_features, train_activity_labels=train_activity_labels,
-                     train_subject_labels=train_subject_labels, test_features=test_features)
+        estimator = nearest_neighbours.KNN(n_neighbors=k)
 
-tuned_parameters = [
-    {'n_neighbors': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
-]
+        train_features, train_activity_labels, train_subject_labels, test_features = trainer.load_data(
+            os.path.join("feature_extraction", '_data_sets/unreduced.pkl'), final=False)
 
-#trainer.find_optimized_model(estimator, train_features, train_subject_labels, tuned_parameters, scorer.auc_evaluator)
+        pandaman.print_stats(train_features=train_features, train_activity_labels=train_activity_labels,
+                             train_subject_labels=train_subject_labels, test_features=test_features)
 
-trainer.evaluate(estimator, train_features, train_subject_labels, scorer.auc_evaluator, results_location)
-visualiser.plot_learning_curves(estimator, train_features, train_subject_labels, results_location)
+
+        trainer.evaluate(estimator, train_features, train_subject_labels, scorer.auc_evaluator, results_location)
+        trainer.evaluate(estimator, train_features, train_subject_labels, scorer.accuracy_evaluator, results_location)
+
+# visualiser.plot_learning_curves(estimator, train_features, train_subject_labels, results_location)
