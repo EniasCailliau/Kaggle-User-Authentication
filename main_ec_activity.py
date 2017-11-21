c05ad6e5f86cda93157038f5c3ea5f42e95901d8
Enias Cailliau <enias.cailliau@outlook.com>
2017-11-21 12:09:59 +0100

    Changed interface of evaluator to enforce correct behavior of evaluation

--- main_ec_activity.py
+++ main_ec_activity.py
@@ -4,14 +4,16 @@ import trainer
 from feature_reduction import feature_reducer
 from model_evaluation import scorer
 from model_evaluation import visualiser
-from models.activity_prediction import gaussianNB
+from models.activity_prediction import gaussianNB, bernoulliNB, svc
 from utils import pandaman, handyman
 
-options = ["ec", "activity", "gaussian", "reduction_LDA"]
+options = ["ec", "activity", "Bernoulli", "not-optimized", "reduction_LDA"]
 
 trainer = trainer.Trainer()
 
-estimator = gaussianNB.GaussianNB()
+# estimator = gaussianNB.GaussianNB()
+estimator = bernoulliNB.Bernoulli()
+# estimator = svc.SVC()
 
 train_features, train_activity_labels, train_subject_labels, test_features = trainer.load_data(
     os.path.join("feature_extraction", '_data_sets/unreduced.pkl'), final=False)
@@ -36,8 +38,10 @@ for i in range(5, 40, 2):
 
     handyman.calculate_path_from_options("Results", options)
 
-    trainer.evaluate(estimator, train_features_reduced, train_activity_labels, scorer.accuracy_evaluator,
+
+    trainer.evaluate(estimator, train_features_reduced, train_activity_labels,
                      results_location)
+
     visualiser.plot_learning_curves(estimator, train_features, train_activity_labels, results_location)
     trainer.save_estimator(estimator, results_location)
     estimator = trainer.load_estimator(results_location)
