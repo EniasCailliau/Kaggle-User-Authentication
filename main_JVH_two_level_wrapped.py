0dec70643869eea30923457a512c8edc82c42bc6
Jeroen Van Hautte <Jeroen@solidsystems.local>
2017-11-24 14:00:17 +0100

    added wrapper

--- main_JVH_two_level_wrapped.py
+++ main_JVH_two_level_wrapped.py
@@ -1,46 +1,28 @@
 import trainer
 import os
-from models import logistic_regression, random_forest, gradient_boosted_trees, two_level
+from models import logistic_regression, random_forest, gradient_boosted_trees, two_level_wrapper
 from model_evaluation import scorer, visualiser
 from feature_reduction import feature_reducer
 import numpy as np
 from utils import pandaman
 import pandas as pd
 
-options = ["JVH", "two_level", "RF-RF"]
+options = ["JVH", "two_level", "RF-RF", "2"]
 results_location = os.path.join("Results", '/'.join(options) + "/")
 # init trainer
 trainer = trainer.Trainer("")
 
-# load data from feature file
+# Load data from feature file
 train_features, train_activity_labels, train_subject_labels, test_features = trainer.load_data(
     os.path.join("feature_extraction", '_data_sets/unreduced.pkl'), final=False)
 
-allUsers = [1, 2, 3, 4, 5, 6, 7, 8]
-allActivities = ['1', '2', '3', '4', '5', '6', '7', '12', '13', '16', '17', '24']
+# Merge features with activity labels (required to use trainer)
+X = pd.DataFrame(np.hstack([train_features.values, train_activity_labels.values.reshape(-1,1)]))
+y = train_subject_labels
+test = pd.DataFrame(np.hstack([test_features.values, np.zeros((test_features.shape[0],1))]))
 
-activityEstimator = random_forest.RF()
+# Set up model
+model = two_level_wrapper.TwoLevelWrapper(results_location)
+print "TESTING"
+trainer.get_acc_auc(model, X, y, results_location)
 
-# assemble lists
-X = []
-userEstimators = []
-
-currentFeatures = feature_reducer.reduce_LDA(train_features, train_activity_labels, 20);
-X.append(currentFeatures)
-trainer.get_acc_auc(activityEstimator, currentFeatures, train_activity_labels, results_location)
-activityEstimator.fit(currentFeatures, train_activity_labels)
-
-for i in range(12): #Split this up at a later time for more control
-    print "Activity " + allActivities[i] +":"
-    currentEstimator = random_forest.RF()
-    currentData = train_features.values[train_activity_labels.values==allActivities[i]]
-    currentLabels = train_subject_labels.values[train_activity_labels.values==allActivities[i]].reshape(-1)
-    currentFeatures = feature_reducer.reduce_LDA(pd.DataFrame(currentData), pd.DataFrame(currentLabels), 20);
-    print currentFeatures.shape
-    trainer.get_acc_auc(currentEstimator, pd.DataFrame(currentFeatures), pd.DataFrame(currentLabels), os.path.join("Results", '/'.join(options) + "/" + allActivities[i] + "/"))
-    currentEstimator.fit(currentFeatures, currentLabels.ravel())
-    userEstimators.append(currentEstimator)
-    X.append(currentFeatures)
-
-model = two_level.TwoLevel(activityEstimator, userEstimators)
-model.predict_proba(X)
--- models/two_level_wrapper.py
+++ models/two_level_wrapper.py
@@ -1,12 +1,14 @@
 import inspect
+import trainer
 
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn import ensemble
 import numpy as np
 import two_level
 from feature_reduction import feature_reducer
+from models import random_forest
+import pandas as pd
 
-PRINT_STATS = False
 CREATED_BY = "Jeroen Van Hautte"
 MODEL_NAME = "Two Level Wrapper"
 # This model wraps around the two level estimator, allowing it to be used more like a regular one
@@ -14,14 +16,16 @@ MODEL_NAME = "Two Level Wrapper"
 
 class TwoLevelWrapper(BaseEstimator):
     # Does nothing
-    def __init__(self):
+    def __init__(self, results_location):
+        self.trainer = trainer.Trainer("")
+        self.results_location = results_location
         return
 
-    # Classifiers are set up here, X should be the unreduced feature set, y should be a 2D array (activity | user)
+    # Classifiers are set up here, X should contain the unreduced feature set and the activity labels, y should be a 1D array containing only user labels
     def fit(self, X, y):
-        train_features = X
-        train_activity_labels = y[:,0]
-        train_subject_labels = y[:, 1]
+        train_features = X[:, 0:len(X[0])-1]
+        train_activity_labels = X[:, len(X[0])-1]
+        train_subject_labels = y
         allUsers = [1, 2, 3, 4, 5, 6, 7, 8]
         allActivities = ['1', '2', '3', '4', '5', '6', '7', '12', '13', '16', '17', '24']
 
@@ -37,33 +41,28 @@ class TwoLevelWrapper(BaseEstimator):
 
         self.reducers.append(currentReducer)
         X_processed.append(currentFeatures)
-
-        if(PRINT_STATS):
-            trainer.get_acc_auc(activityEstimator, currentFeatures, train_activity_labels, results_location)
         activityEstimator.fit(currentFeatures, train_activity_labels)
 
         for i in range(12):  # Split this up at a later time for more control
             print "_____________________________________"
             print "Activity " + allActivities[i] + ":"
             currentEstimator = random_forest.RF()
-            currentData = train_features.values[train_activity_labels.values == allActivities[i]]
-            currentLabels = train_subject_labels.values[train_activity_labels.values == allActivities[i]].reshape(-1)
+            currentData = train_features[train_activity_labels == allActivities[i]]
+            currentLabels = train_subject_labels[train_activity_labels == allActivities[i]].reshape(-1)
             currentReducer = feature_reducer.get_LDA_reducer(pd.DataFrame(currentData), pd.DataFrame(currentLabels), 20)
-            currentFeatures = currentReducer.transform(train_features)
+            currentFeatures = currentReducer.transform(train_features[train_activity_labels == allActivities[i]])
             self.reducers.append(currentReducer)
 
             print currentFeatures.shape
-            if (PRINT_STATS):
-                trainer.get_acc_auc(currentEstimator, pd.DataFrame(currentFeatures), pd.DataFrame(currentLabels),
-                                os.path.join("Results", '/'.join(options) + "/" + allActivities[i] + "/"))
             currentEstimator.fit(currentFeatures, currentLabels.ravel())
             userEstimators.append(currentEstimator)
             X_processed.append(currentFeatures)
         self.estimator = two_level.TwoLevel(activityEstimator, userEstimators)
         return self.estimator
 
-    # Expects X to be the unreduced feature set
+    # Expects X to be the unreduced feature set with an added column that will be discarded
     def predict_proba(self, X):
+        X = X[:, 0:len(X[0]) - 1]
         X_processed = []
         for i in range(13):
             X_processed.append(self.reducers[i].transform(X))
