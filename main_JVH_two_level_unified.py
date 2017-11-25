453028ebd252fcf58cab37cfa21ea6427d841a04
Jeroen Van Hautte <Jeroen@solidsystems.local>
2017-11-25 11:23:31 +0100

    cleanup

--- main_JVH_two_level_unified.py
+++ main_JVH_two_level_unified.py
@@ -1,14 +1,19 @@
 import trainer
 import os
-from models import logistic_regression, random_forest, gradient_boosted_trees, two_level_wrapper
+from models import logistic_regression, random_forest, gradient_boosted_trees, two_level_unified
 from model_evaluation import scorer, visualiser
 from feature_reduction import feature_reducer
 import numpy as np
 from utils import pandaman
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import *
+
+
 
 options = ["JVH", "two_level", "RF-RF2"]
 results_location = os.path.join("Results", '/'.join(options) + "/")
+
 # init trainer
 trainer = trainer.Trainer("")
 
@@ -16,17 +21,15 @@ trainer = trainer.Trainer("")
 train_features, train_activity_labels, train_subject_labels, test_features = trainer.load_data(
     os.path.join("feature_extraction", '_data_sets/unreduced.pkl'), final=False)
 
-print train_features.shape
-print train_activity_labels.shape
-print train_subject_labels.shape
-
 # Merge features with activity labels (required to use trainer)
 X = pd.DataFrame(np.hstack([train_features.values, train_activity_labels.values.reshape(-1,1).astype(int)]))
 y = train_subject_labels
 test = pd.DataFrame(np.hstack([test_features.values, np.zeros((test_features.shape[0],1))]))
 
 # Set up model
-model = two_level_wrapper.TwoLevelWrapper(results_location)
+model = two_level_unified.TwoLevel()
 print "TESTING"
+x_train, x_val, y_train, y_val = train_test_split(X.values, y.values, test_size=0.2)
+
 trainer.get_acc_auc(model, X, y, results_location)
 
