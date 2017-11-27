44398adb97f76c7c433852d274ee42350d9fd453
Louis Scheers <louisscheers@gmail.com>
2017-11-27 23:19:56 +0100

    bug fix and svm rerun

--- main_LS.py
+++ main_LS.py
@@ -4,6 +4,7 @@ from models import svm
 from model_evaluation import scorer, visualiser
 from feature_reduction import feature_reducer
 from utils import pandaman
+import numpy as np
 
 if __name__ == '__main__':
 
@@ -13,9 +14,8 @@ if __name__ == '__main__':
     trainer = trainer.Trainer()
 
     estimator = svm.SVC(class_weight='balanced', C=100, kernel='rbf', gamma=0.0001)
-
-    train_features, train_activity_labels, train_subject_labels, test_features = trainer.load_data(
-        os.path.join("feature_extraction", '_data_sets/unreduced.pkl'), final=False)
+    train_features, train_activity_labels, train_subject_labels, train_session_id, test_features = trainer.load_data(
+        os.path.join("feature_extraction", '_data_sets','unreduced.pkl'), final=False)
 
     # pandaman.print_stats(train_features=train_features, train_activity_labels=train_activity_labels,
     #                      train_subject_labels=train_subject_labels, test_features=test_features)
@@ -32,10 +32,10 @@ if __name__ == '__main__':
       {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['poly'], 'degree': [2, 3, 4]}, # coef0 is best left at 0 for lower degree polys
     #  {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['sigmoid']} # paper suggesting to not use sigmoid kernel
     ]
-    #trainer.find_optimized_model(estimator, train_features, train_subject_labels, tuned_parameters, scorer.auc_evaluator)
+    #trainer.find_optimized_model(estimator, train_features, train_subject_labels, train_session_id, tuned_parameters, scorer.auc_evaluator)
 
-    #trainer.evaluate(estimator, train_features, train_subject_labels, scorer.auc_evaluator, results_location)
-    trainer.get_acc_auc(estimator, train_features, train_subject_labels, results_location)
-    #visualiser.plot_learning_curves(estimator, train_features, train_subject_labels, results_location)
+    print trainer.evaluate(estimator, train_features, train_subject_labels, train_session_id)
+    #visualiser.plot_confusion_matrix(estimator, np.array(train_features.values), train_subject_labels, train_session_id, results_location)
+    #visualiser.plot_learning_curves(estimator, train_features, train_subject_labels, train_session_id, results_location)
 
 
