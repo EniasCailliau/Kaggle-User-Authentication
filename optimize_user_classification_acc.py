2691d4080a368a7adc815115448ad7d760542661
Enias Cailliau <enias.cailliau@outlook.com>
2017-12-06 20:07:16 +0100

    Fixed bug in user classification

--- optimize_user_classification_acc.py
+++ optimize_user_classification_acc.py
@@ -29,11 +29,11 @@ def plot_curves(estimator, results_location, train_labels, train_features, train
 yolo = [{'name': 'n_estimators', 'type': 'discrete', 'domain': (100, 5000, 1)},
         {'name': 'max_depth', 'type': 'discrete', 'domain': (2, 15, 1)},
         {'name': 'min_child_weight', 'type': 'discrete', 'domain': (1, 8, 1)},
-        {'name': 'gamma', 'type': 'continuous', 'domain': (0, 1)},
+        {'name': 'gamma', 'type': 'continuous', 'domain': (1e-5, 1)},
         {'name': 'subsample', 'type': 'continuous', 'domain': (0.6, 1.0)},
         {'name': 'colsample_bytree', 'type': 'continuous', 'domain': (0.6, 1.0)},
         {'name': 'reg_alpha', 'type': 'continuous', 'domain': (1e-5, 100)},
-        {'name': 'learning_rate', 'type': 'continuous', 'domain': (0, 1)},
+        {'name': 'learning_rate', 'type': 'continuous', 'domain': (1e-5, 1)},
         {'name': 'n_folds', 'type': 'discrete', 'domain': (2000, 2000, 1)},
         ]
 
@@ -78,10 +78,10 @@ def generate_folds(train_features, train_activity_labels, train_sessions):
     return folds
 
 
-def evaluate(param, folds, numRounds):
+def evaluate(param, folds, numRounds, n_iter=1):
     auc_scores, acc_scores = [], []
     print("I am using {} rounds".format(numRounds))
-    for i in range(1):
+    for i in range(n_iter):
         xg_train, xg_test, xg_valid, test_Y, test_Y_onehot = folds[i]
         watchlist = [(xg_train, 'train'), (xg_test, 'test')]
         bst = xgb.train(param, xg_train, numRounds, watchlist, early_stopping_rounds=25)
@@ -121,7 +121,6 @@ def xgbCv(x, folds):
 def bayesOpt(folds):
     opt = BayesianOptimization(f=partial(xgbCv, folds=folds),
                                domain=yolo,
-                               num_cores=8,
                                optimize_restarts=15,
                                acquisition_type='MPI',
                                acquisition_weight=0.1,
@@ -151,14 +150,14 @@ def bayesOpt(folds):
     dict_params['subsample'] = params[4]
     dict_params['colsample_bytree'] = params[5]
     dict_params['reg_alpha'] = params[6]
+    dict_params['learning_rate'] = params[7]
     dict_params['objective'] = 'multi:softprob'
     dict_params['num_class'] = 8
     dict_params['silent'] = 1
-
-    print("best params:")
+    print("best params")
     print(dict_params)
 
-    auc_mean, auc_std, acc_mean, acc_std = evaluate(dict_params, folds, int(params[7]))
+    auc_mean, auc_std, acc_mean, acc_std = evaluate(dict_params, folds, int(params[8]), n_iter=4)
     print("I have auc: {} +- {}".format(auc_mean, auc_std))
     print("I have acc: {} +- {}".format(acc_mean, acc_std))
 
@@ -172,7 +171,7 @@ def main():
     # load data from feature file
     train_features, train_activity_labels, train_subject_labels, train_sessions, test_features = trainer.load_data(
         os.path.join("feature_extraction", '_data_sets/augmented.pkl'), final=False)
-    train_subject_labels = train_subject_labels.apply(lambda x: x-1)
+    train_subject_labels = train_subject_labels.apply(lambda x: x - 1)
 
     print_stats(test_features, train_activity_labels, train_features, train_sessions, train_subject_labels)
     global_start_time = datetime.now()
