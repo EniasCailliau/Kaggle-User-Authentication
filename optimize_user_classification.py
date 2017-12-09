ded01fa9cd1f64b193fd57375889ad437bfb177a
Enias Cailliau <enias.cailliau@outlook.com>
2017-12-10 00:53:16 +0100

    added for server

--- optimize_user_classification.py
+++ optimize_user_classification.py
@@ -1,5 +1,8 @@
 import datetime
 import os
+import sys
+import subprocess
+
 from datetime import datetime
 import xgboost as xgb
 from GPyOpt.methods import BayesianOptimization
@@ -12,6 +15,7 @@ from utils import pandaman
 import numpy as np
 from functools import partial
 import matplotlib.pyplot as plt
+from utils import handyman
 
 
 def print_stats(test_features, train_activity_labels, train_features, train_session_id, train_subject_labels):
@@ -27,39 +31,15 @@ def plot_curves(estimator, results_location, train_labels, train_features, train
 
 
 yolo = [{'name': 'n_estimators', 'type': 'discrete', 'domain': (500, 5000, 1)},
-        {'name': 'max_depth', 'type': 'discrete', 'domain': (2, 15, 1)},
+        {'name': 'max_depth', 'type': 'discrete', 'domain': (2, 10, 1)},
         {'name': 'min_child_weight', 'type': 'discrete', 'domain': (1, 8, 1)},
-        {'name': 'gamma', 'type': 'continuous', 'domain': (0, 1)},
+        {'name': 'gamma', 'type': 'continuous', 'domain': (1e-5, 1)},
         {'name': 'subsample', 'type': 'continuous', 'domain': (0.6, 1.0)},
         {'name': 'colsample_bytree', 'type': 'continuous', 'domain': (0.6, 1.0)},
         {'name': 'reg_alpha', 'type': 'continuous', 'domain': (1e-5, 100)},
-        {'name': 'learning_rate', 'type': 'continuous', 'domain': (0, 1)},
+        {'name': 'learning_rate', 'type': 'continuous', 'domain': (1e-5, 1)},
         {'name': 'n_folds', 'type': 'discrete', 'domain': (2000, 2000, 1)},
         ]
-
-
-def send_notification(subject, body):
-    import smtplib
-    from email.MIMEMultipart import MIMEMultipart
-    from email.MIMEText import MIMEText
-
-    fromaddr = "enias.oryx@gmail.com"
-    toaddr = "enias.oryx@gmail.com"
-    msg = MIMEMultipart()
-    msg['From'] = fromaddr
-    msg['To'] = toaddr
-    msg['Subject'] = subject
-
-    body = body
-    msg.attach(MIMEText(body, 'plain'))
-
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(fromaddr, "BILLIE_2")
-    text = msg.as_string()
-    server.sendmail(fromaddr, toaddr, text)
-    server.quit()
-
 
 activity_to_index = {'1': 0, '2': 1, '3': 2, '4': 3, '5': 4, '6': 5, '7': 6, '12': 7, '13': 8, '16': 9, '17': 10,
                      '24': 11}
@@ -78,10 +58,10 @@ def generate_folds(train_features, train_activity_labels, train_sessions):
     return folds
 
 
-def evaluate(param, folds, numRounds, n_iter=1):
+def evaluate(param, folds, numRounds):
     auc_scores, acc_scores = [], []
     print("I am using {} rounds".format(numRounds))
-    for i in [2]:
+    for i in range(4):
         xg_train, xg_test, xg_valid, test_Y, test_Y_onehot = folds[i]
         watchlist = [(xg_train, 'train'), (xg_test, 'test')]
         bst = xgb.train(param, xg_train, numRounds, watchlist, early_stopping_rounds=50)
@@ -89,12 +69,7 @@ def evaluate(param, folds, numRounds, n_iter=1):
         auc = roc_auc_score(test_Y_onehot, y_scores, average='macro')
         print("---- Intermediate score auc: {}".format(auc))
         auc_scores.append(auc)
-
-        y_pred = np.argmax(y_scores, axis=1)
-        acc = accuracy_score(test_Y, y_pred)
-        print("---- Intermediate score acc: {}".format(acc))
-        acc_scores.append(acc)
-    return np.mean(auc_scores), np.std(auc_scores), np.mean(acc_scores), np.std(acc_scores)
+    return np.mean(auc_scores), np.std(auc_scores)
 
 
 def xgbCv(x, folds):
@@ -112,8 +87,9 @@ def xgbCv(x, folds):
         dict_params['objective'] = 'multi:softprob'
         dict_params['num_class'] = 8
         dict_params['silent'] = 1
+        dict_params['n_threads'] = 12
         print(dict_params)
-        auc_mean, auc_std, acc_mean, acc_std = evaluate(dict_params, folds, int(params[8]))
+        auc_mean, auc_std = evaluate(dict_params, folds, int(params[8]))
         fs[i] = auc_mean
     return fs
 
@@ -121,15 +97,12 @@ def xgbCv(x, folds):
 def bayesOpt(folds):
     opt = BayesianOptimization(f=partial(xgbCv, folds=folds),
                                domain=yolo,
-                               num_cores=8,
-                               optimize_restarts=15,
+                               optimize_restarts=5,
                                acquisition_type='EI',
-                               acquisition_weight=2,
-                               batch_size=5,
+                               acquisition_weight=0.2,
                                maximize=True)
 
     opt.run_optimization(max_iter=100, eps=0)
-
 
     print('opt_Y')
     print(opt.Y)
@@ -150,51 +123,32 @@ def bayesOpt(folds):
     dict_params['objective'] = 'multi:softprob'
     dict_params['num_class'] = 8
     dict_params['silent'] = 1
+    dict_params['nthread'] = 12
     print("best params")
     print(dict_params)
 
-    auc_mean, auc_std, acc_mean, acc_std = evaluate(dict_params, folds, int(params[8]), n_iter=4)
+    auc_mean, auc_std = evaluate(dict_params, folds, int(params[8]))
     print("I have auc: {} +- {}".format(auc_mean, auc_std))
-    print("I have acc: {} +- {}".format(acc_mean, acc_std))
-
-    opt.plot_acquisition("acquisition.png")
-    opt.plot_convergence("convergence.png")
 
 
 def main():
-    # try:
-    options = ["JVH", "USER", "XGB", "augmented"]
-    results_location = os.path.join("Results", '/'.join(options) + "/")
+    sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+    tee = subprocess.Popen(["tee", "final_enias_optimisation.txt"], stdin=subprocess.PIPE)
+    os.dup2(tee.stdin.fileno(), sys.stdout.fileno())
+
     # init trainer
     trainer = t.Trainer("")
     # load data from feature file
-    train_features, train_activity_labels, train_subject_labels, train_sessions, test_features = trainer.load_data(
+    _, train_activity_labels, train_subject_labels, train_sessions, test_features = trainer.load_data(
         os.path.join("feature_extraction", '_data_sets/augmented.pkl'), final=False)
     train_subject_labels = train_subject_labels.apply(lambda x: x - 1)
+
+    train_features = handyman.load_pickle("reduced_feature.pkl")
 
     print_stats(test_features, train_activity_labels, train_features, train_sessions, train_subject_labels)
     global_start_time = datetime.now()
 
-    # param = {
-    #     'nrounds': 100000,
-    #     'n_estimators': 200,
-    #     'learning_rate': 0.1,
-    #     'max_depth': 15,
-    #     'min_child_weight': 1,
-    #     'subsample': .7,
-    #     'colsample_bytree': .7,
-    #     'gamma': 0.05,
-    #     'scale_pos_weight': 1,
-    #     'nthread': 8,
-    #     'eta': 0.1
-    # }
-
-
     folds = generate_folds(train_features, train_subject_labels, train_sessions)
-    #
-    # auc_mean, auc_std, acc_mean, acc_std = evaluate(param, folds, 100)
-    # print("I have auc: {} +- {}".format(auc_mean, auc_std))
-    # print("I have acc: {} +- {}".format(acc_mean, acc_std))
 
     print("starting with bayesian optimisation:")
     bayesOpt(folds)
@@ -202,16 +156,6 @@ def main():
     time_elapsed = datetime.now() - global_start_time
 
     print('Time elpased (hh:mm:ss.ms) {}'.format(time_elapsed))
-    send_notification("Finished", 'Time elpased (hh:mm:ss.ms) {}'.format(time_elapsed))
-
-    os.system("say Your program has finished")
-    os.system("say Your program has finished")
-    os.system("say Your program has finished")
-
-
-# except Exception as e:
-#     print(e)
-#     send_notification("Exception occurred", "{}".format(e))
 
 
 if __name__ == '__main__':
